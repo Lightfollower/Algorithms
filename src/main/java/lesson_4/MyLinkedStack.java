f047467c0fe138c7818d0043648778d0955c7ae0
Doom <lightfollower1985@gmail.com>
2019-10-05 11:48:44 +0500

    Скачал ДЗ 4ого урока.

---
+++ src/main/java/lesson_4/MyLinkedStack.java
@@ -0,0 +1,23 @@
+package lesson_4;
+
+public class MyLinkedStack<T> {
+    private MyLinkedList<T> stack = new MyLinkedList<>();
+
+    public void push(T value){
+        stack.insertFirst(value);
+    }
+    public T pop(){
+        return stack.removeFirst();
+    }
+    public T peek(){
+        return stack.getFirst();
+    }
+
+    public int size(){
+        return stack.size();
+    }
+
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+}
